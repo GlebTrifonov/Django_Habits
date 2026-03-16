9f30156492deeb999313b25a339b82f27c447f74
GlebTrifonov <109554143+GlebTrifonov@users.noreply.github.com>
2026-03-16 11:13:35 +0000

    feat: install DRF. Verify Habit API list and create functionality via Browsable API

--- habits/urls.py
+++ habits/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import habit_list, habit_create, habit_toggle, habit_delete
+from .api_views import HabitListAPIView
+
 
 urlpatterns = [
     path('', habit_list, name='habit_list'),
     path('create/', habit_create, name='habit_create'),
     path('<int:pk>/toggle/', habit_toggle, name='habit_toggle'),
     path('<int:pk>/delete/', habit_delete, name='habit_delete'),
+    path('api/v1/list/', HabitListAPIView.as_view(), name='api_habit_list')
 ]
