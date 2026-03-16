d7746731d1737321dc4ef55bf231259f9cea372b
GlebTrifonov <109554143+GlebTrifonov@users.noreply.github.com>
2026-03-16 06:45:08 +0000

    feet: add habits app and Habit model

---
+++ habits/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_comlited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
